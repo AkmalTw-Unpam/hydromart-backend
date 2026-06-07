d2fe950cb87afb4b9ad7a620abe94544aaffca6f
AkmalTirtaWijaya <unpamakmal@gmail.com>
2026-06-07 22:31:48 +0700

    fix production cors

--- config/cors.php
+++ config/cors.php
@@ -1,26 +1,25 @@
 <?php
 
 return [
-    // Izinkan semua jalur API dan cookie Sanctum
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    // JALUR AMAN: Masukkan localhost DAN link domain Vercel Anda di sini
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://hydromart-frontend.vercel.app', // <-- Pindahkan ke sini
+        'https://hydromart-frontend.vercel.app',
     ],
 
-    'allowed_origins_patterns' => [], // <-- Kosongkan saja jika bukan regex
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
-    'max_age' => 86400,
+    'max_age' => 0,
 
-    // Ini wajib TRUE agar token/cookie login bisa dikirim dari React ke Laravel
     'supports_credentials' => true,
+
 ];
