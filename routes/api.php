1fc4a1ed476bf1afd74127aa38694d4e3d155ee5
AkmalTirtaWijaya <unpamakmal@gmail.com>
2026-06-07 22:42:18 +0700

    fix cors and login production

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::options('/{any}', function () {
+    return response()->json([], 200);
+})->where('any', '.*');
+
 use App\Http\Controllers\Api\{
     AuthController, DashboardController, ItemController,
     CategoryController, SupplierController, TransactionController,
