29b735278b4dc2d44a79e20c7dcf66c7d944be08
AkmalTirtaWijaya <unpamakmal@gmail.com>
2026-06-08 21:13:28 +0700

    remove trusthosts

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        \App\Http\Middleware\TrustHosts::class,
+        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
 
         // CORS cukup di GLOBAL saja
